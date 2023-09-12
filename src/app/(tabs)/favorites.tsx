6d952a5cff488820a399ab77415c500d3ea7c186
Jalal Aliyev <jalal.a@crocusoft.com>
2023-09-12 18:46:01 +0400

    created initial app

--- src/app/(tabs)/favorites.tsx
+++ src/app/(tabs)/favorites.tsx
@@ -1,14 +1,18 @@
 import { StyleSheet } from 'react-native';
 
-import EditScreenInfo from '../../components/EditScreenInfo';
 import { Text, View } from '../../components/Themed';
+import EditScreenInfo from '../../components/EditScreenInfo';
 
-export default function TabTwoScreen() {
+export default function FavoritesScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/two.tsx" />
+      <View
+        style={styles.separator}
+        lightColor='#eee'
+        darkColor='rgba(255,255,255,0.1)'
+      />
+      <EditScreenInfo path='app/(tabs)/two.tsx' />
     </View>
   );
 }
